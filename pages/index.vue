9c5ae3dc824104ccc3470aa0e748d2a96150203c
Timmeh <timmygee@users.noreply.github.com>
2020-05-17 00:58:30 +0200

    Add prototype markers

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,20 @@
   <v-row class="">
     <v-col class="pa-0">
       <client-only>
-        <l-map :zoom="zoom" :center="center" :style="`height: ${mapHeight}`">
+        <l-map id="map" :zoom="zoom" :center="center" :style="mapStyle">
           <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
-          <l-marker :lat-lng="center" />
+          <l-marker
+            v-for="(marker, index) in markers"
+            :key="`marker-${index}`"
+            :lat-lng="marker.center"
+          >
+            <l-popup>
+              <p v-if="marker.name" class="font-weight-bold">
+                {{ marker.name }}
+              </p>
+              <p v-if="marker.message" class="">"{{ marker.message }}"</p>
+            </l-popup>
+          </l-marker>
         </l-map>
       </client-only>
     </v-col>
@@ -22,15 +33,44 @@ export default {
   },
   data: () => ({
     zoom: 1,
-    center: [0, 0]
+    center: [0, 0],
+    markers: [
+      {
+        center: [59.93912, 30.31562],
+        name: 'Tim',
+        message: 'We are with you! 🥰'
+      },
+      {
+        center: [59.93892, 30.31762],
+        name: 'Stana',
+        message: 'So much love! ❤️'
+      },
+      {
+        center: [59.93952, 30.31762],
+        name: 'Janina',
+        message: 'So glad I could be here today 😊'
+      },
+      {
+        center: [59.93952, 30.31522],
+        name: 'Christoffer',
+        message: 'Sending solidarity from Deutschland! 💪🏻'
+      }
+    ]
   }),
   computed: {
     mapHeight() {
       return `calc(100vh - ${this.appBarHeight}px)`
+    },
+    mapStyle() {
+      return `height: ${this.mapHeight}`
     },
     ...mapState('app', ['appBarHeight'])
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.leaflet-container {
+  font-size: 16px;
+}
+</style>
