36ca7c663e6cc0de2e28d0c6d5066560f5cd9ccb
Timmeh <timmygee@users.noreply.github.com>
2020-06-05 14:28:19 +0200

    Add dialog dismissal cross

--- components/FormDialog/index.vue
+++ components/FormDialog/index.vue
@@ -5,18 +5,28 @@
     class="form-dialog"
     @click:outside="$store.commit('app/SET_SHOW_FORM_DIALOG', false)"
   >
-    <v-app-bar ref="appBar" color="primary" extension-height="200" flat>
-      <div slot="extension" class="appbar-ext">
-        <h2 class="title font-weight-bold white--text mb-2">
-          {{ $t('dialogs.form.header.title') }}
-        </h2>
-        <h2 class="title white--text mb-2">
-          {{ $t('dialogs.form.header.subtitle') }}
-        </h2>
-        <p class="white--text">
-          {{ $t('dialogs.form.header.text', { prideLocation }) }}
-        </p>
+    <v-app-bar
+      ref="appBar"
+      color="primary"
+      height="200"
+      class="dialog-app-bar"
+      flat
+    >
+      <div
+        class="cross-icon"
+        @click="$store.commit('app/SET_SHOW_FORM_DIALOG', false)"
+      >
+        <CrossSVG />
       </div>
+      <h2 class="title font-weight-bold white--text mb-2">
+        {{ $t('dialogs.form.header.title') }}
+      </h2>
+      <h2 class="title white--text mb-2">
+        {{ $t('dialogs.form.header.subtitle') }}
+      </h2>
+      <p class="white--text">
+        {{ $t('dialogs.form.header.text', { prideLocation }) }}
+      </p>
     </v-app-bar>
     <flag-step v-if="step === steps.FLAG" />
   </v-dialog>
@@ -26,12 +36,14 @@
 import { mapState } from 'vuex'
 import FlagStep from './components/FlagStep'
 import { getKeyMirror } from '~/utils/data'
+import CrossSVG from '~/assets/icons/cross.svg'
 
 const steps = getKeyMirror('FLAG', 'SUBSCRIPTION', 'SHARE')
 
 export default {
   components: {
-    FlagStep
+    FlagStep,
+    CrossSVG
   },
   data: (vm) => ({
     steps,
@@ -46,4 +58,23 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.dialog-app-bar {
+  &::v-deep .v-toolbar__content {
+    flex-direction: column;
+    align-items: start;
+    padding-top: 11px;
+  }
+}
+.cross-icon {
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: 14px;
+  top: 15px;
+  cursor: pointer;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -131,9 +131,17 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {
+      // Optimisation
       if (ctx && ctx.isClient) {
         config.optimization.splitChunks.maxSize = 100000
       }
+      // vue-svg-loader config
+      const svgRule = config.module.rules.find((rule) => rule.test.test('.svg'))
+      svgRule.test = /\.(png|jpe?g|gif|webp)$/
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['babel-loader', 'vue-svg-loader']
+      })
     }
   }
 }
