5851fcc8e1d392b3f4616a1e6b4634a0fcfe0d69
Timmeh <timmygee@users.noreply.github.com>
2020-06-12 13:43:54 +0200

    Remove old marker data

--- test-json/db.js
+++ test-json/db.js
@@ -36,50 +36,21 @@ const range = (start, stop, step) =>
 
 module.exports = () => {
   const data = {
-    attendees: [
-      {
-        _id: 0,
-        center: [59.93912, 30.31562],
-        name: 'Tim',
-        country: 'AU',
-        message: 'We are with you! 🥰'
-      },
-      {
-        _id: 1,
-        center: [59.93892, 30.31762],
-        name: 'Stana',
-        country: 'BU',
-        message: 'So much love! ❤️'
-      },
-      {
-        _id: 2,
-        center: [59.93952, 30.31762],
-        name: 'Janina',
-        country: 'DE',
-        message: 'So glad I could be here today 😊'
-      },
-      {
-        _id: 3,
-        center: [59.93952, 30.31522],
-        name: 'Christoffer',
-        country: 'DE',
-        message: 'Sending solidarity from Deutschland! 💪🏻'
-      }
-    ]
+    attendees: []
   }
   // Create attendees
-  for (let i = 0; i < 1000; i++) {
-    const numEmojis = faker.random.number(emojis.length)
-    data.attendees.push({
-      _id: i,
-      center: [59.93952, 30.31522],
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      country: faker.address.countryCode(),
-      emojiIndices: faker.helpers
-        .shuffle(range(0, numEmojis, 1))
-        .map((index) => index)
-    })
-  }
+  // for (let i = 0; i < 1000; i++) {
+  //   const numEmojis = faker.random.number(emojis.length)
+  //   data.attendees.push({
+  //     _id: i,
+  //     center: [59.93952, 30.31522],
+  //     firstName: faker.name.firstName(),
+  //     lastName: faker.name.lastName(),
+  //     country: faker.address.countryCode(),
+  //     emojiIndices: faker.helpers
+  //       .shuffle(range(0, numEmojis, 1))
+  //       .map((index) => index)
+  //   })
+  // }
   return data
 }
